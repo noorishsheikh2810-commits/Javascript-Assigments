a5fbb0358c98ce9897e838ac2d0c7e49fed24487
NoorishShekh2810-commits <noorisheikh2810@gmail.com>
2026-05-03 09:20:50 -0700

    done

---
+++ Assigment 21-25/app.js
@@ -0,0 +1,221 @@
+
+// Qno:1
+
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your last name:");
+let fullName = firstName + lastName;
+alert(`Hello ${fullName}`);
+
+// Qno:02
+
+let userInput = prompt("Enter your favourite mobile model");
+document.write(`My favourite phone is: ${userInput}`);
+document.write("<br/>");
+document.write(`Length of string is: ${userInput.length}`)
+
+// Qno:03
+
+let str = "Pakistani";
+let newStr =str.indexOf("n");
+document.write(`String ${str}`);
+document.write("<br/>");
+document.write(`Index of 'n' is ${newStr}`);
+
+// Qno:04
+
+let str = "Hello World";
+let newStr =str.lastIndexOf("l");
+document.write(`String ${str}`);
+document.write("<br/>");
+document.write(`Last index of 'l' is ${newStr}`);
+
+// Qno:05
+
+let str = "Pakistani";
+let newStr =str.charAt(3);
+document.write(`String ${str}`);
+document.write("<br/>");
+document.write(`Character at index 3 is ${newStr}`);
+
+// Qno:06
+
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your last name:");
+let fullName = firstName.concat(lastName);
+alert(`Hello ${fullName}`);
+
+// Qno:07
+
+let str = "Hyderabad"
+let str2 = str.replace("Hyderabad", "Islamabad");
+document.write(str)
+document.write("<br/>");
+document.write(str2)
+
+// Qno:08
+
+let message = "Ali and Sami are best friends. They play cricket and football together.";
+let newMessage = message.replace(/and/g, "&");
+document.write(message);
+document.write("<br/>");
+document.write(newMessage);
+
+// Qno:09
+
+let str = "472";
+let newStr = Number(str);
+document.write(`Value: ${str}`);
+document.write("<br/>");
+document.write(`Type: ${typeof str}`);
+document.write("<br/>");
+document.write(`Value: ${newStr}`);
+document.write("<br/>");
+document.write(`Type: ${typeof newStr}`);
+
+// Qno:10
+
+let userInput = prompt("Enter a fruit");
+let upperCase = userInput.toUpperCase();
+document.write(`User input: ${userInput}`);
+document.write("<br/>");
+document.write(`Upper case: ${upperCase}`);
+
+// Qno:11.
+
+let userInput = prompt("Enter a variable");
+let titleCase = userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
+document.write(`User input: ${userInput}`);
+document.write("<br/>");
+document.write(`Title case: ${titleCase}`);
+
+// Qno:12.
+
+let num = 35.36;
+let result = num.toString().replace(".", "");
+document.write(`Number: ${num}`);
+document.write("<br/>");
+document.write(`Result: ${result}`);
+
+// Qno:13.
+
+let userName;
+for (let i = 0; ; i++) {
+  userName = prompt("Enter your username");
+  if (
+    userName.includes("@") ||
+    userName.includes(".") ||
+    userName.includes(",") ||
+    userName.includes("!")
+  ) {
+    alert("Please enter a valid username");
+  } else {
+    alert(`Valid username: ${userName}`);
+    break;
+  }
+}
+
+// Qno:14.
+
+let items = ["cookie", "cake", "apple pie", "chips", "patties"];
+let userInput = prompt(
+  "Welcome to ABC Bakery. What do you want to order Sir/Maam?",
+);
+userInput = userInput.toLowerCase();
+let check = false;
+let i;
+for (i = 0; i <= items.length; i++) {
+  if (userInput === items[i]) {
+    check = true;
+  }
+}
+if (check) {
+  document.write(`${userInput} is available at index ${i} in our bakery`);
+} else {
+  document.write(
+    `We are sorry. ${userInput} is <b>not available</b> in our bakery`,
+  );
+}
+
+// Qno:15.
+
+let password = prompt("Enter your password: ");
+let passwordLength = false;
+let passwordChar = false;
+let passwordInt = false;
+let passwordStart = true;
+
+// Checking for Alphabet
+for (i = 0; i < password.length; i++) {
+  let charValue = password[i].charCodeAt(0);
+
+  // Checking for UpperCase Letters
+  if (charValue >= 65 && charValue <= 90) {
+    passwordChar = true;
+  }
+
+  // Checking for LowerCase Letters
+  else if (charValue >= 97 && charValue <= 122) {
+    passwordChar = true;
+  }
+}
+
+// Checking for Numbers
+for (i = 0; i < password.length; i++) {
+  let charValue = password[i].charCodeAt(0);
+  if (charValue >= 48 && charValue <= 57) {
+    passwordInt = true;
+  }
+}
+
+// Checking for first letter
+let charValue = password.charCodeAt(0);
+if (charValue >= 48 && charValue <= 57) {
+  passwordStart = false;
+}
+
+// Checking for Length
+if (password.length >= 6) {
+  passwordLength = true;
+}
+
+if (
+  passwordChar === false ||
+  passwordInt === false ||
+  passwordLength === false ||
+  passwordStart === false
+) {
+  alert("Enter valid Password");
+} else {
+  alert("Password Approved");
+}
+
+// Qno:16.
+
+let university = "University of Karachi";
+let arr = university.split("");
+for (let i = 0; i < arr.length; i++){
+    document.write(`${arr[i]} <br/>`);
+}
+
+// Qno:17.
+
+let userChar = prompt("Enter a word");
+let lastChar = userChar[userChar.length - 1];
+document.write(`Last character of your word is ${lastChar}`);
+
+// Qno:18.
+
+let str = "The quick brown fox jumps over the lazy dog.";
+
+document.write(`Text: ${str}`);
+document.write(`<br/>`);
+
+let lowerStr = str.toLowerCase();
+let words = lowerStr.split(" ");
+let count = 0;
+
+for (let i = 0; i < str.length; i++){
+    if (words[i] === "the") {
+        count++;
+    }
+}
