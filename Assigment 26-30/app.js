e8b3eb5f23f1ad1eedb5534f5e1748a22c6b9ee2
NoorishShekh2810-commits <noorisheikh2810@gmail.com>
2026-05-23 03:56:14 -0700

    done

--- Assigment 26-30/app.js
+++ Assigment 26-30/app.js
@@ -1,18 +1,80 @@
-function greet(){
-    console.log("hello")
+// function greet (userName) {
+//     alert (`Hello  ${userName} `)
+// }
+
+// greet("arhama")
+
+
+// function fruit(name1 , name2) {
+// console.log(name1 + " " + name2);
+// }
+
+// fruit("Mango", "apple");
+
+
+// let num1 = +prompt("Enter your first number")
+// let num2 = +prompt("Enter your second number")
+// function sum(value1, value2) {
+//     alert(value1 + value2)
+// }
+
+// function multiplication(value1, value2) {
+//     alert(value1 * value2)
+// }
+
+// sum(num1, num2)
+
+// multiplication(num1, num2)
+
+
+let num1 = +prompt("Enter your first number")
+let operator = prompt("Enter your opreator")
+let num2 = +prompt("Enter your second number")
+
+function calculation(value1, value2, opera) {
+    if (opera == "+") {
+        alert(`Sum is ${value1 + value2}`);
+    } else if (opera == "-") {
+        alert(`Minus is ${value1 - value2}`);
+    } else if (opera == "*") {
+        alert(`Multiplication is ${value1 * value2}`);
+    } else if (opera == "/") {
+        alert(`Division is ${value1 / value2}`);
+    } else {
+        alert("Invalid input");
+    }
 }
-greet()
-greet()
-greet()
-greet()
-greet()
+calculation(num1, num2, operator)
 
-// parameter
-function greet(name){
-    console.log("hello" + name)
+
+
+
+    // function decleration
+let add = function (){
+    
 }
 
 
-greet("mehwish"); 
-greet("arhama");
-greet("noorish");
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function greet(name) {
+//   console.log("Hello " + name);
+// }
+
+// function square(num) {
+//   return num * num;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
+
+// function isEven(n) {
+//   return n % 2 === 0;
+// }
+
+// function sayHi() {
+//   console.log("Hi!");
+// }
